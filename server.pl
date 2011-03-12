8a5a2056521ee7243cc5a394431645a367fb2522
Andrew <aparker42@gmail.com>
2011-03-12 17:23:10 +0000

    allow a user to hit / and get the app

--- server.pl
+++ server.pl
@@ -3,6 +3,8 @@
 use Mojolicious::Lite;
 use File::Basename;
 
+get '/' => sub { shift->render_static('ps.html'); };
+
 get '/postscripts' => sub {
     shift->render_json([ map { s/^public\///; $_ } <public/postscripts/*.ps> ]);
 };
